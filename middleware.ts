05723f75f878b17a5c5b22727b5602f3e66d512a
Maca <maca.motions@gmail.com>
2026-04-22 01:28:01 -0400

    fix: add middleware entry point to execute proxy.ts

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { proxy } from './proxy'
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
+
+export const middleware = proxy
